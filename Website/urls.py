8d17b0b36de72c5e4fffabfcedf922ff91e3b06c
Lesley Boyd <lesley.a.boyd1@gmail.com>
2019-01-28 14:56:23 -0600

    added base html and intial templates for other pages

--- Website/urls.py
+++ Website/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
 from . import views
 
-app_name = "website"
+app_name = 'Website'
 urlpatterns = [
-  # path('', views.index, name='index'),
+  path('', views.index, name='index'),
+  path('employees/', views.employees, name='employees'),
+  path('departments/', views.departments, name='departments'),
+  path('computers/', views.computers, name='computers'),
+  path('training/', views.training, name='training'),
 ]
