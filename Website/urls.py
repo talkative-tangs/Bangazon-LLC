86024fd31cc06534b59b0b90ee8c33a79c53c32a
Lesley Boyd <lesley.a.boyd1@gmail.com>
2019-01-29 10:31:05 -0600

    added a computers MVT and computer detail MVT

--- Website/urls.py
+++ Website/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
   path('employees/', views.employees, name='employees'),
   path('departments/', views.departments, name='departments'),
   path('computers/', views.computers, name='computers'),
+  path('computer/<int:computer_id>/', views.computer, name='computer'),
+  path('new_computer/', views.new_computer, name='new_computer')
   path('training/', views.training, name='training'),
 ]
