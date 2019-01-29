385aa866eb22d5e283fda8614c4dacada44293a6
BryanNilsen <bryannilsen@gmail.com>
2019-01-28 22:47:14 -0600

    added sort and filter to training program query

--- Website/views.py
+++ Website/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
 
+from datetime import datetime
+
 from .models import Training_Program
 
 # Create your views here.
@@ -19,9 +21,13 @@ def computers(request):
     return render(request, 'Website/computers.html')
 
 def training(request):
-  ''' Get all of the training programs from DB and send to training.html '''
+  ''' Get all of the training programs from DB,
+  sort by most recent date,
+  filter out programs occurring before today's date,
+  and send to training.html '''
 
-    training_list = Training_Program.objects.all()
-    context = {'training_list': training_list}
+  training_list = Training_Program.objects.all().order_by('start_date').filter(start_date__gt=datetime.today())
 
-    return render(request, 'Website/training.html', context)
+  context = {'training_list': training_list}
+
+  return render(request, 'Website/training.html', context)
