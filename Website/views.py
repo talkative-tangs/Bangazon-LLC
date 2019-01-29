1c930e06b30957390c7a415a4c7801ad551c6484
Elyse Dawson <dawson.elyse@gmail.com>
2019-01-29 12:05:24 -0600

    Employee names listed under employees.html

--- Website/views.py
+++ Website/views.py
@@ -3,15 +3,20 @@ from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
 
+from .models import Employee
+from .models import Department
+
 # Create your views here.
-def index(request):      
+def index(request):
     return render(request, 'Website/index.html')
 
 def employees(request):
-    return render(request, 'Website/employees.html')    
+    employees = Employee.objects.all()
+    context = {'employees': employees}
+    return render(request, 'Website/employees.html', context)
 
 def departments(request):
-    return render(request, 'Website/departments.html')        
+    return render(request, 'Website/departments.html')
 
 def computers(request):
     return render(request, 'Website/computers.html')
