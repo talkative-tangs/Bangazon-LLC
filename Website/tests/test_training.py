3141eab4d6f99f7ef69581d285442de0d19f4a02
BryanNilsen <bryannilsen@gmail.com>
2019-01-30 11:22:01 -0600

    updated training tests

--- Website/tests/test_training.py
+++ Website/tests/test_training.py
@@ -5,7 +5,7 @@ import unittest
 from ..models import Training_Program
 
 class TrainingTest(TestCase):
-      ''' testing suite for Training Programs '''
+      ''' Testing suite for Training Programs '''
       def test_list_training(self):
         new_training =  Training_Program.objects.create(
           program_name='Clerical Orientation',
@@ -21,24 +21,51 @@ class TrainingTest(TestCase):
         # check that the response is 200 ok
         self.assertEqual(response.status_code, 200)
 
-        # check that rendered context contains one item
+        # check that rendered context contains one training program
         self.assertEqual(len(response.context['training_list']), 1)
-
-        #  checks that the response context equals what was sent
-        # self.assertEqual(response.context['training_list'], new_training)
 
         # .encode converts from unicode to utf-8
         self.assertIn(new_training.program_name.encode(), response.content)
-        self.assertIn(new_training.program_desc.encode(), response.content)
-        # self.assertIn('July 11, 2019', response.content)
-        # self.assertIn(new_training.end_date.encode(), response.content)
-        # self.assertIn(new_training.max_attendees.encode(), response.content)
-        # python encodes date string? and integer
-        # print(response.content)
-        print(response.content)
+
+# =================================================
+# Testing Criteria #1
+# =================================================
 
       def test_training_add(self):
+        ''' Your test suite must verify that the content of the responses contain all required input fields, where a form is requested. '''
         response = self.client.get(reverse('Website:training_add'))
-        print(response)
 
-      #   self.assertIn(response.content, 'html output goes here')
+        self.assertIn(
+            '<input id="program_name" type="text" name="program_name" required>'.encode(), response.content)
+        self.assertIn(
+            '<input id="program_desc" type="text" name="program_desc" required>'.encode(), response.content)
+        self.assertIn(
+            '<input id="start_date" type="date" name="start_date" required>'.encode(), response.content)
+        self.assertIn(
+            '<input id="end_date" type="date" name="end_date" required>'.encode(), response.content)
+        self.assertIn(
+            '<input id="max_attendees" type="number" name="max_attendees" required>'.encode(), response.content)
+
+
+# =================================================
+# Testing Criteria 2
+# =================================================
+
+      def test_training_post(self):
+        '''Your test suite must verify that when a POST operation is performed to the corresponding URL, then a successful response is received. Status code must be 200.'''
+
+        response = self.client.post(reverse('Website:training_add'),
+            {
+            'program_name': 'Testing Training',
+            'program_desc': 'Dummy text for the Testing Training program',
+            'start_date': '2019-04-29',
+            'end_date': '2019-05-02',
+            'max_attendees': 11,
+            })
+
+        get_response = self.client.get(reverse('Website:training'))
+
+        # getting 302 back because we have a successful url and the view is redirecting
+        self.assertEqual(response.status_code, 302)
+        # get a 200 when checking the list of training and new training program exists
+        self.assertEqual(get_response.status_code, 200)
