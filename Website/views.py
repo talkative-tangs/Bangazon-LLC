b08fdd0696d45ac4eed8965f07d170adff38af54
Lesley Boyd <lesley.a.boyd1@gmail.com>
2019-01-30 20:29:16 -0600

    added a page for a delete error

--- Website/views.py
+++ Website/views.py
@@ -96,24 +96,19 @@ def computers_delete(request, computer_id):
     '''delete computer from computer list'''
     selected_computer = Computer.objects.get(id=computer_id)
     computer_assignments = Join_Computer_Employee.objects.filter(computer_id=computer_id)
-    
-
-    # if computer_assignments.assign_date == null:
-    #     selected_computer.delete()
-    #     return HttpResponseRedirect(reverse('Website:computers'))
-    # else:
-    #     return HttpResponse('You cannot delete a computer that has been assigned.')
+    print_me = Computer.model
 
     if len(computer_assignments) == 0:
         selected_computer.delete()
         return HttpResponseRedirect(reverse('Website:computers'))
     else:
-        return HttpResponse('You cannot delete a computer that has been assigned.')
+        return render(request,'Website/computers_delete.html')
 
 
     context = {
         'selected_computer': selected_computer,
-        'computer_assignments': computer_assignments
+        'computer_assignments': computer_assignments,
+        'print_me': print_me
         }
     return render(request, 'Website/computers_detail.html', context)
 
